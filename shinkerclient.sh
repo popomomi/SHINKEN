d56a8299984d423a49a3261b127d987641177461
Ngo thanh Long <thanhthi.hn93@gmail.com>
2014-10-07 11:42:43 +0700

    update

--- shinkerclient.sh
+++ shinkerclient.sh
@@ -1,26 +1,16 @@
 #! /bin/bash
 
-echo"--------------Cap nhat he thong ------------------------------------"
+echo "--------------Cap nhat he thong ------------------------------------"
 sudo apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
-
 echo "-------------------------------Cai SNMP------------------------------"
-
-sudo apt-get install snmpd
-
+sudo apt-get -y install snmpd
 echo "---------------------------------------------------------------------"
-
 echo "---------------Chinh sua file cau hinh-------------------------------"
-
 $config=/etc/snmp/snmpd.conf
-
 test -f $config.bka | cp $config $config.bka
-
 echo"----------------------------------------------------------------------"
-
 sed -i "s/agentAddress  udp:127.0.0.1:161/#agentAddress  udp:127.0.0.1:161/g" $config
 sed -i "s/rocommunity public/rocommunity DOmonitoring/g"  $config
-
 echo "---------------------Khoi dong lai  dich vu SNMP-------------------------------"
 service snmpd restart
-
 echo"----------------------KET THUC CAI DAT------------------------------------------"
